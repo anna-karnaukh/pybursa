e11462851d8feea2a2607885a9441967c2e67fd2
Anna Karnaukh <anna.karnaukh1@gmail.com>
2014-11-26 23:27:33 +0200

    HW7_Karnaukh_addition

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,5 +8,9 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^products/$',TemplateView.as_view(template_name='products.html'),name='products'),
+    url(r'^contacts/$',TemplateView.as_view(template_name='contacts.html'),name='contacts'),
+    url(r'^product/$',TemplateView.as_view(template_name='product.html'),name='product'),
+    url(r'^product2/$',TemplateView.as_view(template_name='product2.html'),name='product2'),
     url(r'^admin/', include(admin.site.urls)),
 )
